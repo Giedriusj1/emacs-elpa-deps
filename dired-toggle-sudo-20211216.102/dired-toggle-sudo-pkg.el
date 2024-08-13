cfb236807f60a70efcac0696717f7b590c4b515e
Giedrius <giedriusj1@gmail.com>
2024-08-13 21:14:53 +0100

    Firs commit

---
+++ dired-toggle-sudo-20211216.102/dired-toggle-sudo-pkg.el
@@ -0,0 +1,11 @@
+(define-package "dired-toggle-sudo" "20211216.102" "Browse directory with sudo privileges." 'nil :commit "9f86cdf858225b15c20affb97ed105e4109047bf" :authors
+  '(("Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>"))
+  :maintainers
+  '(("Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>"))
+  :maintainer
+  '("Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>")
+  :keywords
+  '("emacs" "dired"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ npm-20220428.927/npm-pkg.el
@@ -0,0 +1,10 @@
+(define-package "npm" "20220428.927" "Run your npm workflows"
+  '((emacs "25.1")
+    (transient "0.1.0")
+    (jest "20200625"))
+  :commit "6eb0a58274870dd75bf848cf5a916a9f2c6ddae5" :keywords
+  '("tools")
+  :url "https://github.com/shaneikennedy/npm.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
